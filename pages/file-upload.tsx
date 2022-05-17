703edea6e8cfae6c92aed7ba042ba71e5bc98ccc
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2022-05-17 11:40:11 +0800

    uploud

--- pages/file-upload.tsx
+++ pages/file-upload.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Stack, Text } from "@chakra-ui/react";
+import { Button, Input, Progress, Stack, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useState, ChangeEvent } from "react";
 import { Container } from "../Theme/common";
@@ -6,18 +6,28 @@ import { Container } from "../Theme/common";
 export default function DollarDeposits() {
   const [video, setVideo] = useState<any>();
   const [description, setDescription] = useState<any>();
+  const [progress,setProgress] = useState(0)
   const MediaChange = (event: ChangeEvent) => {
     const target = event.target as HTMLInputElement;
     const file: File = (target.files as FileList)[0];
     setVideo(file);
   };
   const handler = () => {
+    console.log("clicked")
     const formData = new FormData();
     formData.append("file", video);
+    console.log(video)
+    formData.append("desc", description);
     axios.post(
-      "",
-      { data: formData },
-      { headers: { "Content-Type": "multipart/form-data" } }
+      "http://localhost:5000",
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" },
+      onUploadProgress: progressEvent => {
+        const t = progressEvent.total
+        const n = progressEvent.loaded
+        const a = Math.round(n/t*100)
+        setProgress(a)
+      } }
     );
   };
   return (
@@ -25,8 +35,9 @@ export default function DollarDeposits() {
       <Stack w="40%" mx="auto">
         <Text color="white">File upload</Text>
         <Input onChange={(e) => MediaChange(e)} color="white" type="file" />
-        <Input onChange={(e) => setDescription(e.target.value)} />
-        <Button color="black" bg="#FFC226">
+        <Input onChange={(e) => setDescription(e.target.value)} color="white" />
+        <Progress hasStripe value={progress} />
+        <Button onClick={handler} color="black" bg="#FFC226">
           Upload
         </Button>
       </Stack>
